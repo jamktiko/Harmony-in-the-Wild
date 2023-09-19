053f3540a2332a6ee457fb522b54946492132145
juttamariia <71065993+juttamariia@users.noreply.github.com>
2023-09-19 07:50:12 +0300

    Update DialogueTrigger.cs

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -6,6 +6,9 @@ public class DialogueTrigger : MonoBehaviour
 {
     [SerializeField] private TextAsset inkJSON;
 
+    // NOTE FOR JUTTA
+    // NOTE EDIT THIS SO THAT THE DIALOGUE IS TRIGGERED BY PRESSING "E" WHEN CLOSE ENOUGH TO THE TARGET
+
     public void TriggerDialogue()
     {
         DialogueManager.GetInstance().StartDialogue(inkJSON);
