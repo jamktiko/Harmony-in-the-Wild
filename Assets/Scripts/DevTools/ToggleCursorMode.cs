3793f14c4a9b77cc88726cf73e93f5dd17f702d0
Zsomczie <zsomi.f@gmail.com>
2023-11-10 18:43:13 +0200

    MainMenu fixed, Skip button added

--- Assets/Scripts/DevTools/ToggleCursorMode.cs
+++ Assets/Scripts/DevTools/ToggleCursorMode.cs
@@ -7,10 +7,21 @@ public class ToggleCursorMode : MonoBehaviour
     private void OnEnable()
     {
         Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     private void OnDisable()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 }
