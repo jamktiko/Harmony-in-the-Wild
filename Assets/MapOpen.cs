874b488af66d2a7d261fe03567dfcc330c18229f
Zsomczie <zsomi.f@gmail.com>
2023-12-01 11:37:57 +0200

    Occulsion culling

--- Assets/MapOpen.cs
+++ Assets/MapOpen.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class MapOpen : MonoBehaviour
 {
     [SerializeField] GameObject map;
+    [SerializeField] GameObject mapcam;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
+            mapcam.SetActive(!mapcam.activeInHierarchy);
             map.SetActive(!map.activeInHierarchy);
         }
     }
