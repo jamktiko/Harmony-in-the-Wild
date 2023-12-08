a28f99ea2b70afa49bb056361a38a66aa576497f
Zsomczie <zsomi.f@gmail.com>
2023-12-08 12:00:43 +0200

    Fox naming and saving

---
+++ Assets/Scripts/Menu/AreYouSureMSG_Naming.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AreYouSureMSG_Naming : MonoBehaviour
+{
+    TMP_Text text;
+    private void Start()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+    void Update()
+    {
+        text.text = "Are you sure you want the name the fox: " + PlayerPrefs.GetString("foxName");
+    }
+}
