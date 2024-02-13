eaea6ef76633f152707cd52e01249c9113130289
Jutta Mujunen <AB5893@student.jamk.fi>
2024-02-13 16:02:36 +0200

    Snow Is Working

--- Assets/Scripts/_Testing/Reindeer.cs
+++ Assets/Scripts/_Testing/Reindeer.cs
@@ -14,28 +14,64 @@ public class Reindeer : MonoBehaviour
 
     private void Start()
     {
-        animator.SetTrigger("walk");
         agent = GetComponent<NavMeshAgent>();
         currentDestination = destinations[0].position;
+
+        StartCoroutine(WalkToDestination());
     }
 
-    private void Update()
+    private void SetNewDestination()
     {
-        agent.SetDestination(currentDestination);
-
-        if(Vector3.Distance(transform.position, currentDestination) < 0.5f)
+        if (currentDestinationIndex < destinations.Count - 1)
         {
-            if(currentDestinationIndex < destinations.Count - 1)
-            {
-                currentDestinationIndex++;
-            }
-
-            else
-            {
-                currentDestinationIndex = 0;
-            }
-
-            currentDestination = destinations[currentDestinationIndex].position;
+            currentDestinationIndex++;
         }
+
+        else
+        {
+            currentDestinationIndex = 0;
+        }
+
+        currentDestination = destinations[currentDestinationIndex].position;
+    }
+
+    private IEnumerator WalkToDestination()
+    {
+        bool nearDestination = false;
+
+        SetNewDestination();
+
+        SetTurnAnimation();
+
+        while (!nearDestination)
+        {
+            agent.SetDestination(currentDestination);
+
+            if (Vector3.Distance(transform.position, currentDestination) < 0.5f)
+            {
+                nearDestination = true;
+            }
+
+            yield return null;
+        }
+
+        StartCoroutine(Idle());
+    }
+
+    private IEnumerator Idle()
+    {
+        animator.SetTrigger("idle");
+
+        yield return new WaitForSeconds(3f);
+
+        StartCoroutine(WalkToDestination());
+    }
+
+    private void SetTurnAnimation()
+    {
+        float direction = transform.position.x - currentDestination.x;
+
+        animator.SetTrigger("walk");
+        animator.SetFloat("turn", direction);
     }
 }
