3cece702b373f6215d7e126e90176544176bc1ba
juttamariia <71065993+juttamariia@users.noreply.github.com>
2024-03-28 14:05:19 +0200

    Terrain Layers + Quest State Dev Tool
    - imported new terrain layers
    - created dev tool to change quest states; only two of them work for now

---
+++ Assets/05_GameBuffs/Realistic Forest Environment Textures/README.txt
@@ -0,0 +1,16 @@
+Hi there!
+
+Thank you for purchasing the 110+ Realistic Forest Environment Textures pack! We hope you like the textures and find them suitable for your project - and if you do, we'd greatly appreciate if you'd leave us a review!
+
+These textures are a small part of our full realistic texture megapack assets found on the Unity Asset Store. If you really like the textures and wish to upgrade to one of the megapacks, a discount has automatically been applied to each of the megapacks should you choose to upgrade!
+
+Megapacks available for upgrade:
+- 2500+ Realistic Textures Megapack: https://prf.hn/l/znvRbPb
+- 1200+ Realistic Nature Textures Megapack: https://prf.hn/l/WJYyNlo
+- 350+ Realistic Nature Textures Megapack: https://prf.hn/l/41OgJQp
+
+Thanks again for your purchase, and good luck with your project! If you need any help with the asset or if you are running into any issues, please don't hesitate to reach out to us via our email gamebuffsgg@gmail.com.
+
+To stay up to date with us and learn more about additional packages we release in the future, you can also follow us on Twitter: https://twitter.com/GameBuffs
+
+- Game Buffs
---
+++ Assets/Scripts/DevTools/DevToolQuestStateChanger.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DevToolQuestStateChanger : MonoBehaviour
+{
+    [SerializeField] private QuestScriptableObject quest;
+    [SerializeField] private TMP_Dropdown dropdown;
+    private string questId;
+    private QuestState currentQuestState;
+
+    private void OnEnable()
+    {
+        questId = quest.id;
+
+        currentQuestState = QuestManager.instance.GetQuestById(questId).state;
+
+        // set the current state of the quest to be shown
+        switch (currentQuestState)
+        {
+            case QuestState.REQUIREMENTS_NOT_MET:
+                dropdown.value = 0;
+                break;
+
+            case QuestState.CAN_START:
+                dropdown.value = 1;
+                break;
+
+            case QuestState.IN_PROGRESS:
+                dropdown.value = 2;
+                break;
+
+            case QuestState.CAN_FINISH:
+                dropdown.value = 3;
+                break;
+
+            case QuestState.FINISHED:
+                dropdown.value = 4;
+                break;
+        }
+    }
+
+    public void ChangeQuestState()
+    {
+        switch (dropdown.value)
+        {
+            case 2:
+                GameEventsManager.instance.questEvents.StartQuest(questId);
+                break;
+
+            case 4:
+                GameEventsManager.instance.questEvents.FinishQuest(questId);
+                break;
+
+            default:
+                Debug.Log("No changes coded for this state type.");
+                break;
+        }
+    }
+}
