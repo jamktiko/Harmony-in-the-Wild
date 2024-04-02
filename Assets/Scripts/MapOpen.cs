2fc540fc85c6ab8bfea8b9856dbf3d1507838d3e
Jeremy L <112544082+LeBlancJS88@users.noreply.github.com>
2024-04-02 21:52:11 +0300

    Refactored MapOpener script
    Applied clean coding principles to MapOpener.CS to extract functionalities to methods for readability. Introduced functionality to hold reference to post processing profile, check existence of Depth of Field on start and report status to Console, disable DepthOfField on Map open, and re-enable DepthOfField on Map close.

--- Assets/Scripts/MapOpen.cs
+++ Assets/Scripts/MapOpen.cs
@@ -1,36 +1,84 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class MapOpen : MonoBehaviour
 {
     [SerializeField] GameObject mapPanel;
     [SerializeField] GameObject mapCam;
+    [SerializeField] internal Volume globalVolume;
+
+    private UnityEngine.Rendering.Universal.DepthOfField depthOfField;
+
+    void Start()
+    {
+        CheckDepthOfField();
+    }
+
+    private void CheckDepthOfField()
+    {
+        if (globalVolume.profile.TryGet(out depthOfField))
+        {
+            Debug.Log("Depth of Field found in the Global Volume.");
+        }
+        else
+        {
+            Debug.LogError("Depth of Field not found in the Global Volume.");
+        }
+    }
 
     void Update()
     {
+        HandleMapToggle();
+        HandleDebugFeatures();
+    }
+
+    private void HandleMapToggle()
+    {
         if (Input.GetKeyDown(KeyCode.M))
         {
-            mapCam.SetActive(!mapCam.activeInHierarchy);
-            mapPanel.SetActive(!mapPanel.activeInHierarchy);
-
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-
-            if (!mapCam.activeInHierarchy)
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
+            ToggleMapVisibility();
+            UpdateCursorState();
+            ToggleDepthOfField();
         }
+    }
 
-        //unlock and lock all tp points for testing purposes wew
+    private void ToggleMapVisibility()
+    {
+        mapCam.SetActive(!mapCam.activeInHierarchy);
+        mapPanel.SetActive(!mapPanel.activeInHierarchy);
+    }
+
+    private void UpdateCursorState()
+    {
+        bool mapIsActive = mapCam.activeInHierarchy;
+        Cursor.lockState = mapIsActive ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = mapIsActive;
+    }
+
+    private void ToggleDepthOfField()
+    {
+        if (depthOfField != null)
+        {
+            depthOfField.active = !mapCam.activeInHierarchy;
+        }
+    }
+
+    private void HandleDebugFeatures()
+    {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            foreach (Transform child in mapPanel.transform)
+            ToggleDebugFeatures();
+        }
+    }
+
+    private void ToggleDebugFeatures()
+    {
+        foreach (Transform child in mapPanel.transform)
+        {
+            if (child.name != "Map")
             {
-                if (child.name != "Map")
-                {
-                    child.gameObject.SetActive(!child.gameObject.activeInHierarchy);
-                }
+                child.gameObject.SetActive(!child.gameObject.activeInHierarchy);
             }
         }
     }
