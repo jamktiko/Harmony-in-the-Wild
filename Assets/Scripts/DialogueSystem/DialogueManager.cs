85c2b4734768a5e812bbd3ea70d6fb2e8a028b12
juttamariia <71065993+juttamariia@users.noreply.github.com>
2024-04-11 12:16:22 +0300

    Update DialogueManager.cs

--- Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Ink.Runtime;
-using Ink.UnityIntegration;
 using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
