f46c0857d007b4f6ba70496649b391ff712275c5
Jeremy L <112544082+LeBlancJS88@users.noreply.github.com>
2024-04-15 21:33:14 +0300

    Dungeon Entrance VFX Implementation to Penguin Dungeon

--- Assets/Scripts/Dungeons/General/DungeonInstructionPanel.cs
+++ Assets/Scripts/Dungeons/General/DungeonInstructionPanel.cs
@@ -1,12 +1,42 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class DungeonInstructionPanel : MonoBehaviour
 {
+    [SerializeField] private VisualEffect dungeonEntranceVFX;
+    private int onDungeonStartID;
+
+    private void Awake()
+    {
+        onDungeonStartID = Shader.PropertyToID("OnDungeonStart");
+
+        // Check if the dungeonEntranceVFX is not assigned in the inspector
+        if (dungeonEntranceVFX == null)
+        {
+            // Find the DungeonEntrance VFX by its name in the hierarchy
+            var dungeonEntranceObject = GameObject.Find("DungeonEntrance");
+            if (dungeonEntranceObject != null)
+            {
+                dungeonEntranceVFX = dungeonEntranceObject.GetComponent<VisualEffect>();
+
+                if (dungeonEntranceVFX == null)
+                {
+                    Debug.LogWarning("VisualEffect component not found on DungeonEntrance object!");
+                }
+            }
+            else
+            {
+                Debug.LogError("DungeonEntrance object not found in the hierarchy!");
+            }
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.X))
         {
             Invoke(nameof(HideInstructionPanel), 0.1f);
+            dungeonEntranceVFX.SendEvent("OnDungeonStart");
         }
     }
 
