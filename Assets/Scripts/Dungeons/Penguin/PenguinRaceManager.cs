59436554669426ab54ae2465f4127da1710bb321
Zsombor Faragó <zsomi.f@gmail.com>
2024-06-19 14:58:24 +0300

    nvm, made a new branch for VS

--- Assets/Scripts/Dungeons/Penguin/PenguinRaceManager.cs
+++ Assets/Scripts/Dungeons/Penguin/PenguinRaceManager.cs
@@ -89,7 +89,7 @@ public class PenguinRaceManager : MonoBehaviour
     {
         yield return new WaitForSeconds(3f);
 
-        StorybookHandler.instance.SetNewStorybookData(storybookSectionIndex, "OverWorld_VS", true);
+        StorybookHandler.instance.SetNewStorybookData(storybookSectionIndex, "Overworld", true);
         SceneManager.LoadScene("Storybook");
     }
 }
