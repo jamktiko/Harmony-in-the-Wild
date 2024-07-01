ff583cbccaef4f207ea95467d07c7fafed8d4f4c
Zsomczie <zsomi.f@gmail.com>
2024-07-01 12:59:17 +0300

    Merge

--- Assets/Code/Scripts/Quests/Tutorial/ExitCaveTrigger.cs
+++ Assets/Code/Scripts/Quests/Tutorial/ExitCaveTrigger.cs
@@ -26,23 +26,28 @@ public class ExitCaveTrigger : MonoBehaviour
     }
     IEnumerator LoadSceneWithLoadingScreenWithText2(int sceneId)
     {
+        Debug.Log(SaveManager.instance.gameData.playerPositionData);
         GameObject screen = Instantiate(loadingScreen,GameObject.FindGameObjectWithTag("Canvas").transform);
         loadingScreenText = screen.GetComponentInChildren<TMP_Text>();
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+        
         while (!operation.isDone)
         {
             float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
             if (progressValue < 0.33f)
             {
                 loadingScreenText.text = "Loading.";
+                Debug.Log(SaveManager.instance.gameData.playerPositionData);
             }
             else if (progressValue < 0.66f)
             {
                 loadingScreenText.text = "Loading..";
+                Debug.Log(SaveManager.instance.gameData.playerPositionData);
             }
             else if (progressValue > 0.66f)
             {
                 loadingScreenText.text = "Loading...";
+                Debug.Log(SaveManager.instance.gameData.playerPositionData);
             }
             yield return null;
         }
