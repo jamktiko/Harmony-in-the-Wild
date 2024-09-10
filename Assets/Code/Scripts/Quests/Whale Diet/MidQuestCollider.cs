aa3b5dcdb220542f58d64edf30bfbcf95966c916
juttamariia <71065993+juttamariia@users.noreply.github.com>
2024-09-10 13:25:49 +0300

    Whale Quest Colliders
    - added mid-quest and pre-quest colliders to Whale Quest

---
+++ Assets/Code/Scripts/Quests/Whale Diet/MidQuestCollider.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidQuestCollider : MonoBehaviour
+{
+    [SerializeField] private QuestScriptableObject quest;
+
+    private QuestState currentState;
+
+    private void Start()
+    {
+        CheckQuestProgressStatus();
+
+        if (currentState != QuestState.FINISHED)
+        {
+            return;
+        }
+
+        else
+        {
+            DisableQuestColliders();
+        }
+    }
+
+    private void OnEnable()
+    {
+        GameEventsManager.instance.questEvents.OnFinishQuest += UpdateQuestProgressStatus;
+    }
+
+    private void OnDisable()
+    {
+        GameEventsManager.instance.questEvents.OnFinishQuest -= UpdateQuestProgressStatus;
+    }
+
+    private void CheckQuestProgressStatus()
+    {
+        currentState = QuestManager.instance.CheckQuestState(quest.id);
+    }
+
+    private void DisableQuestColliders()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    private void UpdateQuestProgressStatus(string id)
+    {
+        if(id == quest.id)
+        {
+            CheckQuestProgressStatus();
+
+            if (currentState == QuestState.FINISHED && transform.childCount > 0)
+            {
+                DisableQuestColliders();
+            }
+        }
+    }
+}
