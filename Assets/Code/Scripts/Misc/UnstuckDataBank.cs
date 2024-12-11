2759d444c412672460c5d583a415d60d883d108a
juttamariia <71065993+juttamariia@users.noreply.github.com>
2024-12-11 13:59:53 +0200

    Unstuck Draft for Water Edge Issue

---
+++ Assets/Code/Scripts/Misc/UnstuckDataBank.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnstuckDataBank : MonoBehaviour
+{
+    public static UnstuckDataBank instance;
+
+    [SerializeField] private Transform unstuckRescuePointParent;
+    [SerializeField] private Transform player;
+
+    private List<Transform> rescuePoints = new List<Transform>();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else
+        {
+            Debug.LogWarning("Multiple Unstuck Data Banks in the scene!");
+        }
+
+        CreateRescuePointList();
+    }
+
+    private void CreateRescuePointList()
+    {
+        foreach (Transform child in unstuckRescuePointParent)
+        {
+            rescuePoints.Add(child);
+        }
+    }
+
+    public List<Transform> GetRescuePoints()
+    {
+        return rescuePoints;
+    }
+
+    public Transform GetPlayer()
+    {
+        return player;
+    }
+}
---
+++ Assets/Code/Scripts/Misc/UnstuckRescue.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnstuckRescue : MonoBehaviour
+{
+    private Vector3 nearestRescuePoint;
+    private List<Transform> rescuePoints = new List<Transform>();
+    private Transform player;
+
+    private void Start()
+    {
+        if (!UnityEngine.SceneManagement.SceneManager.GetActiveScene().name.Contains("Overworld", System.StringComparison.CurrentCultureIgnoreCase))
+        {
+            gameObject.SetActive(false);
+        }
+
+        GetNeededData();
+    }
+
+    private void GetNeededData()
+    {
+        rescuePoints = UnstuckDataBank.instance.GetRescuePoints();
+        player = UnstuckDataBank.instance.GetPlayer();
+    }
+
+    public void Unstuck()
+    {
+        nearestRescuePoint = FindNearestRescuePoint();
+        MovePlayerToSafeLocation();
+    }
+
+    private Vector3 FindNearestRescuePoint()
+    {
+        Vector3 newPosition = new Vector3();
+        float smallestDistance = -1f;
+
+        foreach (Transform rescuePoint in rescuePoints)
+        {
+            float distanceToPlayer = Vector3.Distance(player.position, rescuePoint.position);
+
+            if(distanceToPlayer < smallestDistance || smallestDistance == -1f)
+            {
+                smallestDistance = distanceToPlayer;
+                newPosition = rescuePoint.position;
+            }
+        }
+
+        return newPosition;
+    }
+
+    private void MovePlayerToSafeLocation()
+    {
+        FoxMovement.instance.gameObject.SetActive(false);
+        player.position = nearestRescuePoint;
+        FoxMovement.instance.gameObject.SetActive(true);
+    }
+}
