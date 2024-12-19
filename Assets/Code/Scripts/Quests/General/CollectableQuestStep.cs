dfefd34cde8da61f416bcce5e997ba84d21bd0dc
juttamariia <71065993+juttamariia@users.noreply.github.com>
2024-12-19 12:50:50 +0200

    Whale Quest Fix AGAIN HIHI
    - apples should not be present if they have already been collected during the quest

--- Assets/Code/Scripts/Quests/General/CollectableQuestStep.cs
+++ Assets/Code/Scripts/Quests/General/CollectableQuestStep.cs
@@ -1,13 +1,23 @@
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class CollectableQuestStep : QuestStep
 {
     public int itemsCollected = 0;
     private int itemToComplete = 5;
+    private List<bool> itemStates = new List<bool>();
+    private List<GameObject> apples = new List<GameObject>();
 
     private void Start()
     {
-        GameEventsManager.instance.questEvents.ShowQuestUI(GetQuestId(), objective, progress + " " + itemsCollected + "/" + itemToComplete);
+        InitializeQuestUI();
+
+        if(SceneManager.GetActiveScene().name.Contains("Overworld", System.StringComparison.CurrentCultureIgnoreCase))
+        {
+            SetAppleObjects();
+            InitializeItemStates();
+        }
     }
 
     private void OnEnable()
@@ -24,31 +34,112 @@ public class CollectableQuestStep : QuestStep
     {
         if (scene.name.Contains("Overworld", System.StringComparison.CurrentCultureIgnoreCase))
         {
-            GameEventsManager.instance.questEvents.ShowQuestUI(GetQuestId(), objective, progress + " " + itemsCollected + "/" + itemToComplete);
+            InitializeQuestUI();
+            SetAppleObjects();
+            InitializeItemStates();
         }
     }
 
-    public void CollectableProgress()
+    private void InitializeQuestUI()
     {
+        GameEventsManager.instance.questEvents.ShowQuestUI(GetQuestId(), objective, progress + " " + itemsCollected + "/" + itemToComplete);
+    }
+
+    public void CollectableProgress(int itemIndex)
+    {
+        // don't proceed if the index is falsely set or the corresponding item has already been collected
+        if(itemIndex < 0 || itemIndex >= itemToComplete || itemStates[itemIndex])
+        {
+            Debug.Log("Invalid item index or corresponding apple has already been collected.");
+            return;
+        }
+
+        itemStates[itemIndex] = true;
         itemsCollected++;
+
         UpdateState();
         GameEventsManager.instance.questEvents.UpdateQuestProgressInUI(progress + " " + itemsCollected + "/" + itemToComplete);
 
         if (itemsCollected >= itemToComplete)
         {
+            // hide remaining apples
+            foreach(GameObject apple in apples)
+            {
+                apple.SetActive(false);
+            }
+
             FinishQuestStep();
         }
     }
 
+    private void SetAppleObjects()
+    {
+        apples = AppleDataHolder.instance.GetApples();
+
+        if(apples != null)
+        {
+            for(int i = 0; i < apples.Count; i++)
+            {
+                apples[i].SetActive(!itemStates[i]);
+            }
+        }
+
+        else
+        {
+            Debug.LogWarning("Was not able to set up apple visibility in Whale Diet, apple list was null.");
+        }
+    }
+
+    private void InitializeItemStates()
+    {
+        foreach(GameObject apple in apples)
+        {
+            itemStates.Add(false);
+        }
+    }
+
+    private List<bool> ParseItemStates(string stateString)
+    {
+        List<bool> states = new List<bool>();
+        string[] stateValues = stateString.Split(',');
+
+        foreach (var value in stateValues)
+        {
+            states.Add(bool.Parse(value));
+        }
+
+        return states;
+    }
+
     private void UpdateState()
     {
-        string state = itemsCollected.ToString();
+        string state = itemsCollected + ";" + string.Join(",", itemStates);
         ChangeState(state);
     }
 
     protected override void SetQuestStepState(string state)
     {
-        itemsCollected = System.Int32.Parse(state);
+        if (string.IsNullOrEmpty(state))
+        {
+            InitializeItemStates();
+            return;
+        }
+
+        string[] stateParts = state.Split(";");
+
+        // if there was previously only one saved value (itemsCollected in older version of the game), initialize the apple values to prevent errors
+        if(stateParts.Length == 1)
+        {
+            itemsCollected = int.Parse(stateParts[0]);
+            InitializeItemStates();
+        }
+
+        else
+        {
+            itemsCollected = itemsCollected = int.Parse(stateParts[0]);
+            itemStates = ParseItemStates(stateParts[1]);
+        }
+
         UpdateState();
     }
 }
--- Assets/Code/Scripts/Quests/Whale Diet/AppleCheck.cs
+++ Assets/Code/Scripts/Quests/Whale Diet/AppleCheck.cs
@@ -10,7 +10,7 @@ public class AppleCheck : MonoBehaviour
     {
         QuestState questState = QuestManager.instance.CheckQuestState(questSO.id);
 
-        if(questState == QuestState.FINISHED)
+        if(questState == QuestState.FINISHED || QuestManager.instance.GetQuestById(questSO.id).GetCurrentQuestStepIndex() > 0)
         {
             gameObject.SetActive(false);
         }
