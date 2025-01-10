65728f380abfd7176861881f80c013c15f95e6d3
Multiainen <peter.arndt.suomi@gmail.com>
2025-01-10 03:33:32 +0200

    Updated/fixed telegrab VFX
    Improved effect to render in proper 3D and account for target object distance. Fixed effect firing on awake.
    
    Added some WIP swimming VFX assets in preparation.

---
+++ Assets/Art/VFX/VFX_Subgraphs/VFXGetTransformDir.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class VFXGetTransformDir : MonoBehaviour
+{
+    // Gets flat transform velocity & directional angle of transform movement for Y rotation of horizontal billboard trails
+    public VisualEffect vfx;
+    private Vector2 oldPos = Vector3.zero;
+    private float angle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x != oldPos.x || transform.position.z != oldPos.y)
+        {
+            vfx.SetVector2("DirVector", (new Vector2(transform.position.x, transform.position.z) - oldPos).normalized);
+            vfx.SetFloat("DirAngle", Vector2.SignedAngle(new Vector2(transform.position.x, transform.position.z) - oldPos, Vector2.right));
+            vfx.SetFloat("TransformVelocity", (new Vector2(transform.position.x, transform.position.z) - oldPos).magnitude / Time.deltaTime);
+        }
+        oldPos = new Vector2(transform.position.x, transform.position.z);
+    }
+}
