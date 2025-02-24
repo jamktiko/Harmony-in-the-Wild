86e1f4144744cfb4e697d5eab54dd4a3f4cc9e7c
juttamariia <71065993+juttamariia@users.noreply.github.com>
2025-02-24 17:40:53 +0200

    Bone To Pick Improvements
    - NavMeshMovement almost ready; works, but not pretty to look at

---
+++ Assets/Code/Scripts/Quests/Bone to Pick/NavMeshNPCMovement.cs
@@ -0,0 +1,163 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshNPCMovement : MonoBehaviour
+{
+    [Header("Quest Config")]
+    [SerializeField] private DialogueQuestNPCs character;
+    [SerializeField] private QuestScriptableObject questSO;
+    [SerializeField] private int questStepIndexToAllowMovement;
+
+    [Header("Movement Config")]
+    [SerializeField] private float maxDistanceToPlayer = 10f;
+    [SerializeField] private List<Transform> destinations;
+
+    [Header("Other Needed References")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject interactionIndicator;
+    [SerializeField] private Transform player;
+
+    private Vector3 currentTargetDestination;
+    private int currentTargetDestinationIndex;
+    private bool canMove;
+    private bool playerIsNear;
+    private NavMeshAgent agent;
+    private Coroutine idleCoroutine;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        CheckInitialSpawnPoint();
+    }
+
+    private void OnEnable()
+    {
+        GameEventsManager.instance.questEvents.OnStartMovingQuestNPC += EnableMovement;
+    }
+
+    private void OnDisable()
+    {
+        GameEventsManager.instance.questEvents.OnStartMovingQuestNPC -= EnableMovement;
+    }
+
+    private void Update()
+    {
+        if (canMove && playerIsNear)
+        {
+            idleCoroutine = null;
+            MoveNPC();
+        }
+
+        else if(canMove && !playerIsNear && idleCoroutine == null)
+        {
+            Debug.Log("starting to idle");
+            idleCoroutine = StartCoroutine(Idle());
+        }
+
+        CheckDistanceToPlayer();
+    }
+
+    private void MoveNPC()
+    {
+        agent.SetDestination(currentTargetDestination);
+
+        if (Vector3.Distance(transform.position, currentTargetDestination) < 1.5f)
+        {
+            SetNewDestination();
+        }
+    }
+
+
+
+    private void EnableMovement(DialogueQuestNPCs questCharacterToMove)
+    {
+        if(character == questCharacterToMove)
+        {
+            if (QuestManager.instance.GetQuestById(questSO.id).GetCurrentQuestStepIndex() == questStepIndexToAllowMovement)
+            {
+                canMove = true;
+
+                if (animator != null)
+                {
+                    animator.SetTrigger("walk");
+                }
+
+                else
+                {
+                    Debug.LogError($"No animator attached to { gameObject.name }!");
+                }
+
+                if (interactionIndicator != null)
+                {
+                    interactionIndicator.SetActive(false);
+                }
+
+                else
+                {
+                    Debug.LogError($"No interaction indicator attached to { gameObject.name }!");
+                }
+            }
+        }
+    }
+
+    private void SetNewDestination()
+    {
+        // if the list contains a new destination, set it is current destination
+        // otherwise use the first destination as a target to start a new loop
+        if (currentTargetDestinationIndex < destinations.Count - 1)
+        {
+            Debug.Log("setting new destination");
+            currentTargetDestinationIndex++;
+
+            currentTargetDestination = destinations[currentTargetDestinationIndex].position;
+        }
+
+        else
+        {
+            Debug.Log("final destination reached");
+
+            GameEventsManager.instance.questEvents.ReachTargetDestinationToCompleteQuestStep(questSO.id);
+            canMove = false;
+
+            if (interactionIndicator != null)
+            {
+                interactionIndicator.SetActive(true);
+            }
+
+            else
+            {
+                Debug.LogError($"No interaction indicator attached to { gameObject.name }!");
+            }
+        }
+    }
+
+    private void CheckInitialSpawnPoint()
+    {
+        if (QuestManager.instance.CheckQuestState(questSO.id) == QuestState.CAN_FINISH)
+        {
+            transform.position = destinations[destinations.Count - 1].position;
+        }
+
+        else
+        {
+            currentTargetDestination = destinations[0].position;
+        }
+    }
+
+    private void CheckDistanceToPlayer()
+    {
+        playerIsNear = Vector3.Distance(transform.position, player.position) <= maxDistanceToPlayer;
+    }
+
+    private IEnumerator Idle()
+    {
+        // set animation to idle
+
+        yield return new WaitUntil(() => playerIsNear);
+
+        // set animation to move
+    }
+}
