7b386c13d893095cfc74de6e797b5802e8ef5bdc
Jeremy LeBlanc <112544082+LeBlancJS88@users.noreply.github.com>
2025-02-25 18:17:11 +0200

    Merge conflict resolution

---
+++ Assets/Art/VFX/Fox Exiting Water/WaterExitEventController.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class WaterExitEventController : MonoBehaviour
+{
+    [SerializeField] public VisualEffect vfxGraph;
+    internal string beginExitWaterEventName = "OnExitWater";
+    internal string shakeWaterLeftEventName = "OnShakeWaterLeft";
+    internal string shakeWaterRightEventName = "OnShakeWaterRight";
+    internal string endExitWaterEventName = "OnWaterExited";
+
+    public void Awake()
+    {
+        vfxGraph.SendEvent(endExitWaterEventName);
+    }
+
+    public void BeginExitWater()
+    {
+            vfxGraph.SendEvent(beginExitWaterEventName);
+    }
+
+    public void ShakeWaterLeft()
+    {
+        if (vfxGraph != null)
+        {
+            vfxGraph.SendEvent(shakeWaterLeftEventName);
+        }
+        else
+        {
+            Debug.LogWarning("VFX Graph reference is missing!");
+        }
+    }
+
+    public void ShakeWaterRight()
+    {
+        if (vfxGraph != null)
+        {
+            vfxGraph.SendEvent(shakeWaterRightEventName);
+        }
+        else
+        {
+            Debug.LogWarning("VFX Graph reference is missing!");
+        }
+    }
+
+    public void EndExitWater()
+    {
+        if (vfxGraph != null)
+        {
+            vfxGraph.SendEvent(endExitWaterEventName);
+        }
+        else
+        {
+            Debug.LogWarning("VFX Graph reference is missing!");
+        }
+    }
+}
